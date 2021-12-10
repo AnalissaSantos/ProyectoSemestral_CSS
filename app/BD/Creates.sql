6e1aac3de769ca173aea1fffcc62d529e6dd13f6
Ana Saa <anagsaam@gmail.com>
2021-12-10 11:19:27 -0500

    cambios a la creacion de la Base de datos

--- app/BD/Creates.sql
+++ app/BD/Creates.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE CitaWebDB;
 
 CREATE TABLE Paciente (
-    N_CedulaAsegurado VARCHAR(20) NOT NULL,
     N_CedulaPaciente VARCHAR(20) NOT NULL,
     N_Seguro_Pac VARCHAR(20),
     Nombre_A_Pac VARCHAR(30) NOT NULL,
@@ -10,21 +9,25 @@ CREATE TABLE Paciente (
     Apellido_B_Pac VARCHAR(30),
     Date_Nacimiento_Pac DATE NOT NULL,
     Email_Pac VARCHAR(30) NOT NULL,
-    Constrasena_Pac VARCHAR(30) NOT NULL,
+    --Constrasena_Pac VARCHAR(30) NOT NULL, --va contraseña? --no va
+    N_CedulaAsegurado VARCHAR(20) NOT NULL,
 
-    CONSTRAINT Pac_Ced_PK PRIMARY KEY (N_CedulaAsegurado)
+    CONSTRAINT Pac_Ced_PK 
+        PRIMARY KEY (N_CedulaPaciente)
 
 );
 
 CREATE TABLE Paciente_Contac(
-    N_CedulaAsegurado VARCHAR(20) NOT NULL,
-    ID_ContacNum_Pac INTEGER NOT NULL AUTO_INCREMENT,
-    Etiqueta_Contac_Pac VARCHAR(12) NOT NULL,
+    N_CedulaPaciente VARCHAR(20) NOT NULL
+        CONSTRAINT N_CedulaPaciente_fk
+            FOREIGN KEY (N_CedulaPaciente) -- primary 
+            REFERENCES Paciente(N_CedulaPaciente),
     Numero_Contac_Pac VARCHAR(12) NOT NULL,
+
 #   Keys
-    PRIMARY KEY (ID_ContacNum_Pac),
-    FOREIGN KEY (N_CedulaAsegurado)
-        REFERENCES Paciente(N_CedulaAsegurado)
+    CONSTRAINT ID_ContacNum_Pac_N_CedulaPaciente_PK
+        PRIMARY KEY (ID_ContacNum_Pac, N_CedulaPaciente)
+    
 );
 
 CREATE TABLE Especialidad(
@@ -36,15 +39,17 @@ CREATE TABLE Especialidad(
 
 CREATE TABLE Referencia(
     N_Referencia INTEGER NOT NULL auto_increment,
-    N_CedulaAsegurado VARCHAR(20) NOT NULL,
+    N_CedulaPaciente VARCHAR(20) NOT NULL,
     ID_Especialidad INTEGER NOT NULL,
     Descripcion_Ref LONGTEXT,
 #     Keys
     CONSTRAINT Ref_NRef_PK PRIMARY KEY (N_Referencia),
-    CONSTRAINT Ref_Pac_NCed_FK FOREIGN KEY (N_CedulaAsegurado)
-        REFERENCES Paciente(N_CedulaAsegurado),
+    CONSTRAINT Ref_Pac_NCed_FK FOREIGN KEY (N_CedulaPaciente)
+        REFERENCES Paciente(N_CedulaPaciente),
     CONSTRAINT Ref_Espec_IDEspec_FK FOREIGN KEY (ID_Especialidad)
         REFERENCES Especialidad (ID_Especialidad)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
 );
 # Policlinicas
 #
@@ -56,7 +61,7 @@ CREATE TABLE Policlinica(
     PRIMARY KEY (ID_Polic)
 );
 
-CREATE TABLE Policlinica_Horario(
+/*CREATE TABLE Policlinica_Horario(
     ID_Horario_Polic INTEGER NOT NULL auto_increment,
     ID_Polic INTEGER NOT NULL,
     Nombre_Dia VARCHAR(12),
@@ -64,7 +69,7 @@ CREATE TABLE Policlinica_Horario(
 
     PRIMARY KEY (ID_Horario_Polic),
     FOREIGN KEY (ID_Polic) REFERENCES Policlinica(ID_Polic)
-);
+);*/
 # PERSONAL------------------------------------------------------
 
 CREATE TABLE Personal(
@@ -79,48 +84,55 @@ CREATE TABLE Personal(
     CONSTRAINT Pers_ID_PK PRIMARY KEY (ID_Personal)
 );
 
-# CREATE TABLE Administrador(
-#     ID_Personal INTEGER NOT NULL auto_increment,
-# #     Keys
-#     CONSTRAINT Adm_Pers_IDPer_FK FOREIGN KEY (ID_Personal)
-#         REFERENCES Personal(ID_Personal),
-#     CONSTRAINT Adm_IDAdm_PK PRIMARY KEY (ID_Personal)
-# );
 
 CREATE TABLE Doctor(
     ID_Doctor VARCHAR(12) NOT NULL,
-    ID_Personal INTEGER NOT NULL,
-    ID_Polic INTEGER NOT NULL,
-    FOREIGN KEY (ID_Polic) references Policlinica(ID_Polic),
-    CONSTRAINT Doc_Pers_IDPers_FK FOREIGN KEY (ID_Personal)
-                   REFERENCES Personal (ID_Personal),
-    CONSTRAINT Doct_PK PRIMARY KEY (ID_Doctor,ID_Personal)
+    ID_Personal INTEGER NOT NULL
+        CONSTRAINT Doctor_ID_Personal_FK FOREIGN KEY (ID_Personal)
+            REFERENCES Personal (ID_Personal),
+    ID_Polic INTEGER NOT NULL
+        FOREIGN KEY (ID_Polic) references Policlinica(ID_Polic)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE,
+    ID_Especialidad INTEGER NOT NULL
+        CONSTRAINT Doctor_ID_Especialidad_FK
+            FOREIGN KEY (ID_Especialidad)
+                references Especialidad (ID_Especialidad)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+    
+    CONSTRAINT ID_Personal PRIMARY KEY (ID_Personal)
 );
 
 CREATE TABLE Cita_Estado(
-    ID_Estado_Cita INTEGER NOT NULL AUTO_INCREMENT,
-    Nombre_Cita_Est VARCHAR(12) NOT NULL,
-#    atendido, cancelado,
+    ID_Estado_Cita INTEGER NOT NULL AUTO_INCREMENT
+        CONSTRAINT Cita_ID_Estado_Cita_PK 
+            PRIMARY KEY (ID_Estado_Cita),
+    Nombre_Cita_Est VARCHAR(12) NOT NULL, --atendido, cancelado,
 #   Keys
-    CONSTRAINT Cita_Est_PK PRIMARY KEY (ID_Estado_Cita)
+    
 );
 
 CREATE TABLE Cita(
     N_Cita INTEGER NOT NULL AUTO_INCREMENT,
-    ID_Doctor VARCHAR(12) NOT NULL,
-    ID_Especialidad INTEGER NOT NULL,
-    N_CedulaAsegurado VARCHAR(20) NOT NULL,
-    ID_Estado_Cita INTEGER NOT NULL,
     Fecha_Cita DATE NOT NULL,
     Is_Control BOOLEAN NOT NULL,
-#   Keys
-    CONSTRAINT Cita_Doct_ID_FK FOREIGN KEY (ID_Doctor)
-                REFERENCES Doctor(ID_Doctor),
-    CONSTRAINT Cita_Espec_CitaID_FK FOREIGN KEY (ID_Especialidad)
-                REFERENCES Especialidad(ID_Especialidad),
-    CONSTRAINT Cita_Pac_NumCed_FK FOREIGN KEY (N_CedulaAsegurado)
-                REFERENCES Paciente(N_CedulaAsegurado),
-    CONSTRAINT Cita_CitaEst_ID_FK FOREIGN KEY (ID_Estado_Cita)
+    hora_cita time not null,
+    ID_Personal INTEGER NOT NULL
+        CONSTRAINT Cita_Doct_ID_FK FOREIGN KEY (ID_Personal)
+                REFERENCES Doctor(ID_Personal), --doctor
+    ID_Especialidad INTEGER NOT NULL
+        CONSTRAINT Cita_ID_Especialidad_FK FOREIGN KEY (ID_Especialidad)
+                REFERENCES Especialidad(ID_Especialidad)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+    N_CedulaPaciente VARCHAR(20) NOT NULL
+        CONSTRAINT Cita_N_CedulaPaciente_fk FOREIGN KEY (N_CedulaPaciente)
+                REFERENCES Paciente(N_CedulaPaciente),
+    ID_Estado_Cita INTEGER NOT NULL
+        CONSTRAINT Cita_ID_Estado_Cita_FK FOREIGN KEY (ID_Estado_Cita)
                 REFERENCES Cita_Estado (ID_Estado_Cita),
-    CONSTRAINT Cita_PK PRIMARY KEY (N_Cita,ID_Doctor,N_CedulaAsegurado)
+    
+#   Keys
+    CONSTRAINT Cita_PK PRIMARY KEY (N_Cita, ID_Personal, N_CedulaPaciente)
 );
