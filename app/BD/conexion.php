d8b28bddcf002ff2eba7a615c1b410a10d585282
Ana Saa <anagsaam@gmail.com>
2021-12-11 01:45:25 -0500

    merger con archivos de Jose y reestructuracion

--- app/BD/conexion.php
+++ app/BD/conexion.php
@@ -1,7 +1,7 @@
 <?php
-@$conexion=mysqli_connect("mysql","root","secret","CitaWebDB");
-//if ($conexion){
-//    print "Conexion BD exitosa";
-//}else{
-//    print "Conexion BD mal";
-//}
+@$conexion=mysqli_connect("localhost","id18102313_root",">H0QHyiWl%%(5Ayl","id18102313_citawebdb");
+/*if ($conexion){
+    print "Conexion BD exitosa";
+}else{
+    print "Conexion BD mal";
+}*/
