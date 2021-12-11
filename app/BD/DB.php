4f516e746003b943d499dc608b59c2326e9c1175
@jmendez08 <mendez8079@gmail.com>
2021-12-11 00:11:40 -0500

    bd

--- app/BD/DB.php
+++
@@ -1,7 +0,0 @@
-<?php
-@$connection=mysqli_connect("mysql","root","mendezdb","CitaWebDB");
-if ($connection){
-    print "Conexion BD exitosa";
-}else{
-    print "Conexion BD mal";
-}
---
+++ app/BD/conexion.php
@@ -0,0 +1,7 @@
+<?php
+@$conexion=mysqli_connect("mysql","root","secret","CitaWebDB");
+//if ($conexion){
+//    print "Conexion BD exitosa";
+//}else{
+//    print "Conexion BD mal";
+//}
